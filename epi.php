71c2edf1a163302f638abcb78b2a63df43992ca6
mbuliga <39514942+mbuliga@users.noreply.github.com>
2019-02-13 23:18:07 +0200

    needed for the site

---
+++ epi.php
@@ -0,0 +1,10 @@
+a:active, a:link, a:visited		{color: rgb(255, 102, 0); font-weight: bold; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif; text-decoration: none }
+a:hover	{color: #336699; font-weight: bold; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif; text-decoration: none }
+
+body 			{ color: rgb(102, 102, 102)}
+p			{ color: rgb(102, 102, 102); font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif }
+td			{ color: rgb(102, 102, 102); font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif }
+th			{ color: rgb(102, 102, 102); font-weight: bold; font-size: 16px; font-family: Verdana, Helvetica, Arial, Geneva, Swiss, SunSans-Regular, sans-serif; text-align: center }
+h1                      { color: rgb(102, 153, 204); font-size: 1.2em; font-weight: bold; margin: 0; padding-top: 4px; padding-bottom: 4px; }
+h2                      { color: red; font-size: 1.2em; font-weight: bold; margin: 0; padding-top: 4px; padding-bottom: 4px }
+h3                      { color: #555555; font-size: 1.2em; font-weight: bold; margin: 0; padding-top: 4px; padding-bottom: 4px; clear: both }
